ab4f4593dceadc2633f2e9a317081574e0e2061c
Sergio Moreno <sergio.moreno@flowable.com>
2021-03-19 13:13:18 +0100

    feat: introduced example template

--- README.md
+++
@@ -1,2 +0,0 @@
-# Redux-Made-Easy-with-Rematch
-Redux Made Easy with Rematch, published by Packt
---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => (
+  <div className="w-full max-w-8xl mx-auto min-h-screen">
+    header
+    <div className="grid sm:grid-cols-1 md:grid-cols-0/5 grid-rows-1 h-full">
+      <div>product list</div>
+      <div>cart</div>
+    </div>
+  </div>
+);
+
+export default App;
